0c3a1703fff8f806ad57555bb72aa5465ead4bb2
smtnou <56159819+smtnou@users.noreply.github.com>
2019-10-04 19:21:49 +0700

    1.01

---
+++ test.js
@@ -0,0 +1,27 @@
+var countDownEnd = 8000;
+		var countDownStart = 0;
+
+		// Update the count down every 1 second
+		var x = setInterval(function() {
+		  countDownStart += 1000;
+
+		  // Find the distance
+		  var distance = countDownEnd - countDownStart;
+		  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		  // Display the result in the element with id="timerLabel"
+		  document.getElementById("timerLabel").innerHTML = minutes + "m " + seconds + "s ";
+
+		  if (distance < 5001 && distance > 4999) {
+		  	document.getElementById("alertTab").style.display = "block";
+		  }
+
+		  if (distance < 0) {
+		    clearInterval(x);
+		    document.getElementById("timerLabel").innerHTML = "Hết giờ!";
+		    //document.getElementById("testForm").sub
+		    //document.getElementById("btnSubmit").click();
+		    window.open("https://www.facebook.com/sangmaitennguoiou/", "_self");
+		  }
+		}, 1000);
